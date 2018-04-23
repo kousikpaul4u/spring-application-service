5ef19bbb0977009d28c7fe4b6d087f08a446af2a
kpal <koushik.pal@mrcooper.com>
2018-04-23 11:55:27 +0530

    initial commit

---
+++ src/main/java/io/koushik/jobservice/JobServiceApplication.java
@@ -0,0 +1,26 @@
+package io.koushik.jobservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class JobServiceApplication {
+
+	@Value("${service.instance.name}")
+	private String instance;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JobServiceApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String get() {
+		return "Hello from: " + instance;
+	}
+}
